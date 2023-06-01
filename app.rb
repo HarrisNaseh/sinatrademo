f421a34f14f3609fad4cb1a4822d06be7db6e013
Harris Naseh <hnaseh@harris-mbp.hsd1.va.comcast.net>
2023-06-01 13:41:06 -0400

    Can return json when in /cakes.json

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require 'json'
 get '/' do
   a = []
   File.foreach("cake.list") {|line| a.push(line.chomp)}
-  a.join("<br>")
-  
+  a.join("<br>")#joins all the array objects into a single string with <br> in between each
+end
+
+get '/cakes.json' do
+  a = []
+  File.foreach("cake.list") {|line| a.push(line.chomp)}
+  return a.to_json# returns a single json string with all the cakes
 end
